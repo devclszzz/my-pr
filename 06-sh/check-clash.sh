429004d13cf5502b02218b6f305dbf44cb3e0cde
devclszzz <devclszzz@gmail.com>
2022-12-10 15:05:30 +0800

    -- update

---
+++ 06-sh/check-clash.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+curl ifconfig.io
+if [ 0 -ne $? ]; then
+    docker rm -f adg
+    docker run -d --net=host --name=adg --restart=always registry.cn-shenzhen.aliyuncs.com/jmglezhu/mydocker:adg-cn-3999-53-2
+fi
+
+curl 127.0.0.1:9090/providers/proxies
+if [ 0 -eq $? ]; then
+    count=`docker ps | grep adg-cn-3999-53-clash -c`
+    if [ 0 -eq $count ]; then
+        docker rm -f adg
+        docker run -d --net=host --name=adg --restart=always registry.cn-shenzhen.aliyuncs.com/jmglezhu/mydocker:adg-cn-3999-53-clash
+    fi
+fi
